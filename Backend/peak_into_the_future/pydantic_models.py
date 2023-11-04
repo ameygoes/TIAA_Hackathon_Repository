991889be5569af2f503ab32e135a800bcee84e37
Rohan Awhad <rohanavad007@gmail.com>
2023-11-04 10:07:33 -0700

    added path model

--- Backend/peak_into_the_future/pydantic_models.py
+++ Backend/peak_into_the_future/pydantic_models.py
@@ -1,8 +1,13 @@
 from pydantic import BaseModel
 
+class Path(BaseModel):
+    title: str
+    description: str
 
 class ActionModel(BaseModel):
     title: str
     description: str
     action: str
     age: int
+    path_type: Path
+    delta_in_retirement_savings: int
